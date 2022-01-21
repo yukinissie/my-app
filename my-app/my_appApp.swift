d237b1bc1920337ab83116a8e9d4380f17740d25
yukinissie <bigtree2490@outlook.jp>
2022-01-22 00:14:11 +0900

    Initial Commit

---
+++ my-app/my_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_appApp.swift
+//  my-app
+//
+//  Created by 西勇樹 on 2022/01/22.
+//
+
+import SwiftUI
+
+@main
+struct my_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
